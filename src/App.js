62dfd45bd3ce1840857d06332ac73086ed9f564f
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-05-20 02:07:30 -0500

    --

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import User from './components/User';
 function App() {
   return (
     <div>
-      <Router basename="/github-finder">
+      <Router>
         <AnimatePresence mode='wait'>
           <Routes>
             <Route path="/" element={<Search />} />
