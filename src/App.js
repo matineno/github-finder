d84cd75d1c37599abea23003f736b6c41466002d
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-05-20 01:22:46 -0500

    made some changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import User from './components/User';
 function App() {
   return (
     <div>
-      <Router>
+      <Router basename="/github-finder">
         <AnimatePresence mode='wait'>
           <Routes>
             <Route path="/" element={<Search />} />
